bc13aa2de3e3a59e2289ddf7fa15e99f95d1a65a
Ziyaad <xavierbaatjes@yahoo.com>
2020-08-05 19:47:49 +0200

    Update smashing-plugin.php

--- smashing-plugin.php
+++ smashing-plugin.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: Smashing Plugin
 	Description: This is for updating your Smashing Plugin to the latest version.
-	Version: 3.0.7
+	Version: 3.1.0
 	Tested: 5.4.2
 	Author: XavierB
 	Author URI: https://www.entertainaholic.com/
@@ -20,13 +20,20 @@ $updater->set_repository( 'smashing-plugin' );
 $updater->initialize();
 	
 
-	
-// Register the options/settings page
-function smashing_options_page() {
-	add_menu_page('Smashing Dashboard', 'Smashing Dashboard', 'manage_options', 'smashing-plugin', 'smashing_options_page');
+// Register the fields
+function smashing_register_settings() {
+	add_option( 'smashing_option_apikey', '');
+	add_option( 'smashing_option_check', '');
+	register_setting( 'smashing_options_group', 'smashing_option_apikey', 'smashing_callback' );
+	register_setting( 'smashing_options_group', 'smashing_option_check', 'smashing_callback' );
 }
-add_action('admin_menu', 'smashing_options_page');
+add_action( 'admin_init', 'smashing_register_settings' );
 
+// Register the options/settings page
+function smashing_register_options_page() {
+	add_menu_page('Smashing Dashboard', 'Smashing Dashboard', 'manage_options', 'smashing', 'smashing_options_page');
+}
+add_action('admin_menu', 'smashing_register_options_page');
 
 // The options/settings page
 function smashing_options_page()
@@ -35,5 +42,5 @@ function smashing_options_page()
 	if ( ! current_user_can( 'manage_options' ) ) {
 		return;
 	}
-	echo 'The Smashing options page!';
+	echo 'Smashing Options Page!';
 }
