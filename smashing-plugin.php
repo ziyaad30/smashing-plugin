68e5f1010365f02979b3192ddeb61b2146d176fb
Ziyaad <xavierbaatjes@yahoo.com>
2020-08-05 19:20:23 +0200

    Update smashing-plugin.php

--- smashing-plugin.php
+++ smashing-plugin.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: Smashing Plugin
 	Description: This is for updating your Smashing Plugin to the latest version.
-	Version: 3.0.5
+	Version: 3.0.6
 	Tested: 5.4.2
 	Author: XavierB
 	Author URI: https://www.entertainaholic.com/
@@ -17,4 +17,23 @@ $updater->set_repository( 'smashing-plugin' );
 /*
 	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
 */
-$updater->initialize();
+$updater->initialize();.
+	
+
+	
+// Register the options/settings page
+function smashing_options_page() {
+	add_menu_page('Smashing Dashboard', 'Smashing Dashboard', 'manage_options', 'smashing-plugin', 'smashing_options_page');
+}
+add_action('admin_menu', 'smashing_options_page');
+
+
+// The options/settings page
+function smashing_options_page()
+{
+	// check user capabilities
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return;
+	}
+	echo 'The Smashing options page!';
+}
