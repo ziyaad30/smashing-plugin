ec59bf4ccc966fd32a61093eeb6c6c21236e09f9
Ziyaad <xavierbaatjes@yahoo.com>
2020-08-05 15:30:49 +0200

    Update smashing-plugin.php

--- smashing-plugin.php
+++ smashing-plugin.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: Smashing Plugin
 	Description: This is for updating your Wordpress plugin.
-	Version: 1.0.2
+	Version: 1.0.0
 	Author: XavierB
 	Author URI: http://www.matthewray.com
 */
