2ee0486d81dd368fd10e0fb617ec055d74826b6a
Ziyaad <xavierbaatjes@yahoo.com>
2020-08-05 16:59:47 +0200

    Update smashing-plugin.php

--- smashing-plugin.php
+++ smashing-plugin.php
@@ -3,6 +3,7 @@
 	Plugin Name: Smashing Plugin
 	Description: This is for updating your Smashing Plugin to the latest version.
 	Version: 3.0.1
+	Tested: 5.4.2
 	Author: XavierB
 	Author URI: https://www.entertainaholic.com/
 */
